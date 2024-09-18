af08115739926dbef81e0e05c2e9ac94bfd837ea
Gregory H Eales <gregory.hamilton.e@gmail.com>
2024-09-17 20:10:46 -0700

    tidy up

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ fn load_image(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
 		let image = image::open(file_path)?;	
 		println!("img size: {:?}", image.dimensions());	
 		let mut out : Vec<String> = vec![];	
-    let mut curr_pool : Vec<&u32> = vec![];
     let mut curr_string : String = "".to_string();
     for x in 0..250 {
         for y in 0..250 {
@@ -15,10 +14,6 @@ fn load_image(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     }     
         
 		println!("length of out vec: {}", out.len());	
-		// need to create a mapping between pixels -> ascii -> pixels 
-		// img -> text -> img 
-		// might need some kind of edge detection for character seleciton
-		// need to decide pixel pooling to ascii character
     
     for s in out.iter() {
         println!(" {} {} ", s, "");
@@ -34,9 +29,6 @@ fn get_magnitude(a : u32, b : u32, c : u32) -> u32 {
 fn get_ascii_char(img: &dyn GenericImageView<Pixel = image::Rgba<u8>>, i: u32, j: u32, size: u32) -> Option<char> {
     let ascii_char = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^`\'.";	
     let num_char = ascii_char.len();
-    // loop through square (size x size) starting from i, j
-    // map find magnitude of color vector avg across pixels
-    // map intensity (avg mag) to char via ascii str (brightest -> darkest) 
     let mut vals : Vec<u32> = vec![];
     let max_mag = (3.0*(225_i32).pow(2) as f32).sqrt();
     for a in 0..size {
@@ -44,7 +36,7 @@ fn get_ascii_char(img: &dyn GenericImageView<Pixel = image::Rgba<u8>>, i: u32, j
             if b + j >= img.dimensions().1 || a + i >= img.dimensions().0 {
                 continue;
             } 
-            let mut pixel = img.get_pixel(i + a, j + b).0;
+            let pixel = img.get_pixel(i + a, j + b).0;
             vals.push(get_magnitude(pixel[0] as u32, pixel[1] as u32, pixel[2] as u32));   
         }
     } 
@@ -56,7 +48,6 @@ fn get_ascii_char(img: &dyn GenericImageView<Pixel = image::Rgba<u8>>, i: u32, j
     avg = avg / vals.len() as u32;
     
     avg = (num_char as f32 * avg as f32  / max_mag) as u32;
-    //println!("avg of pixels: {}", avg);
     if avg > 1 {
         avg = avg - 1;
     }
